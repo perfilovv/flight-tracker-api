ebca9ae40da52142d81e5a1653b8f5eef0356b42
VP <russianspiritoneshotlol@gmail.com>
2026-08-08 21:30:10 +0300

    fix: update database URL configuration to use zod schema validation

--- backend-nest/drizzle.config.ts
+++ backend-nest/drizzle.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'drizzle-kit';
 import 'dotenv/config';
-import { config } from 'src/config/env.config';
+import z from 'zod';
+
+const migrationEnvSchema = z.object({
+  DATABASE_URL: z.url(),
+});
+
+const env = migrationEnvSchema.parse(process.env);
 
 export default defineConfig({
   schema: './src/modules/**/*.schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: config.databaseUrl,
+    url: env.DATABASE_URL,
   },
   verbose: true,
   strict: true,
