f8c0effa4d924b6a5e6a67f4af15ca4cc69b83a0
VP <russianspiritoneshotlol@gmail.com>
2026-08-08 21:47:16 +0300

    fix: refactor database and Redis modules to use provider classes for connection management

--- backend-nest/src/database/database.module.ts
+++ backend-nest/src/database/database.module.ts
@@ -1,18 +1,32 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Logger, Module, OnModuleDestroy } from '@nestjs/common';
 import { Pool } from 'pg';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import 'dotenv/config';
 import * as schema from './schema';
 import { config } from '../config/env.config';
 
+@Injectable()
+export class PgPoolProvider implements OnModuleDestroy {
+  private readonly logger = new Logger(PgPoolProvider.name);
+  public readonly pool: Pool;
+
+  constructor() {
+    this.pool = new Pool({ connectionString: config.databaseUrl });
+  }
+
+  async onModuleDestroy() {
+    this.logger.log('Closing Postgres pool');
+    await this.pool.end();
+  }
+}
+
 @Module({
   providers: [
+    PgPoolProvider,
     {
       provide: Pool,
-      useFactory: () =>
-        new Pool({
-          connectionString: config.databaseUrl,
-        }),
+      inject: [PgPoolProvider],
+      useFactory: (provider: PgPoolProvider) => provider.pool,
     },
     {
       provide: 'DRIZZLE',
--- backend-nest/src/redis/redis.module.ts
+++ backend-nest/src/redis/redis.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Logger, Module, OnModuleDestroy } from '@nestjs/common';
 import { config } from 'src/config/env.config';
 import Redis from 'ioredis';
 
+@Injectable()
+export class RedisClientProvider implements OnModuleDestroy {
+  private readonly logger = new Logger(RedisClientProvider.name);
+  public readonly client: Redis;
+
+  constructor() {
+    this.client = new Redis(config.redisUrl);
+  }
+
+  async onModuleDestroy() {
+    this.logger.log('Closing Redis connection');
+    await this.client.quit();
+  }
+}
 @Module({
   providers: [
-    { provide: 'REDIS_CLIENT', useFactory: () => new Redis(config.redisUrl) },
+    RedisClientProvider,
+    {
+      provide: 'REDIS_CLIENT',
+      inject: [RedisClientProvider],
+
+      useFactory: (provider: RedisClientProvider) => provider.client,
+    },
   ],
   exports: ['REDIS_CLIENT'],
 })
