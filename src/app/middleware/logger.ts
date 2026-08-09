2c66870fe8c56e05640e453becb0b99a6549ce22
VP <russianspiritoneshotlol@gmail.com>
2026-08-09 14:24:44 +0300

    fix: repo

--- src/app/middleware/logger.ts
+++
@@ -1,8 +0,0 @@
-import type { IncomingMessage, ServerResponse } from 'node:http';
-import type { Middleware } from './types.ts';
-
-export const logger: Middleware = (req: IncomingMessage, _res: ServerResponse, next: () => void) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
--- src/modules/flights/routes/flights.routes.ts
+++
@@ -1,8 +0,0 @@
-import { registerRoute } from '../../../app/router/index.ts';
-import { createFlightController, getFlightController, getFlightsController } from '../controller/flights.controller.ts';
-
-registerRoute('GET', '/api/flights', getFlightsController);
-
-registerRoute('GET', '/api/flights/:id', getFlightController);
-
-registerRoute('POST', '/api/flights', createFlightController);
