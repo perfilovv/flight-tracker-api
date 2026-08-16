f2acd2e2ecef24d5d2a40efa75ce91d81fecdbdd
VP <russianspiritoneshotlol@gmail.com>
2026-08-16 20:13:45 +0300

    feat: add .env.example file and update README with environment variables and deployment instructions

--- backend-nest/src/flights/flights.controller.ts
+++ backend-nest/src/flights/flights.controller.ts
@@ -12,10 +12,27 @@ import { CreateFlightDto } from './dto/create-flight.dto';
 import { FlightsService } from './flights.service';
 import { RateLimitGuard } from 'src/shared/guards/rate-limit.guard';
 import { UpdateFlightDto } from './dto/update-flight.dto';
+import { Flight } from './entities/flight.entity';
 
 @Controller('flights')
 export class FlightsController {
   constructor(private readonly flightsService: FlightsService) {}
+
+  @Get()
+  findAll() {
+    return this.flightsService.findAll({});
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: string) {
+    return this.flightsService.getById(id);
+  }
+
+  @Get('stats')
+  getStats() {
+    return this.flightsService.getStats();
+  }
+
   @UseGuards(JwtAuthGuard, RateLimitGuard)
   @Post()
   create(@Body() dto: CreateFlightDto) {
@@ -23,14 +40,17 @@ export class FlightsController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Get()
-  findAll() {
-    return this.flightsService.findAll({});
-  }
-
-  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: UpdateFlightDto) {
     return this.flightsService.update(id, dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id/status/:status')
+  updateStatus(
+    @Param('id') id: string,
+    @Param('status') status: Flight['status'],
+  ) {
+    return this.flightsService.updateStatus(id, status);
   }
 }
